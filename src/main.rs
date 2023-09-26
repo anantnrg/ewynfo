f3451c5ca72fc44a6bc64b7e840b31bab2a6f798
Anant Narayan <anantnrg@gmail.com>
2023-09-26 17:55:44 +0530

    :sparkles: Added network status

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use clap::Parser;
 use linecount::count_lines;
-use std::{
-	io,
-	process::{
-		Command,
-		Stdio,
-	},
+use std::process::{
+	Command,
+	Stdio,
 };
 use sysinfo::{
 	CpuExt,
@@ -31,22 +28,32 @@ fn main() {
 fn parse_args(args: Arguments, sys: System) {
 	match args.device.as_str() {
 		"cpu" => {
-			if args.info.as_str() == "usage" {
+			if args.info == "usage" {
 				get_cpu_usage(sys);
-			} else if args.info.as_str() == "info" {
-				todo!();
+			} else {
+				println!("Unknown info requested")
 			}
 		}
 		"memory" | "ram" => {
-			if args.info.as_str() == "usage" {
+			if args.info == "usage" {
 				get_memory_usage(sys);
 			}
 		}
 		"os" => {
-			if args.info.as_str() == "updates" {
+			if args.info == "updates" {
 				get_updates();
-			} else if args.info.as_str() == "distro" {
+			} else if args.info == "distro" {
 				get_distro(sys)
+			}
+		}
+		"network" => {
+			if args.info == "status" {
+				let status = get_network_status();
+				if status == 1 {
+					println!("1");
+				} else {
+					println!("0");
+				}
 			}
 		}
 		&_ => {
@@ -76,6 +83,30 @@ fn get_updates() {
 	println!("{}", pkg_count);
 }
 
-fn get_distro(mut sys: System) {
+fn get_distro(sys: System) {
 	println!("{}", sys.distribution_id());
+}
+
+fn get_network_status() -> i32 {
+	let servers = ["archlinux.org", "github.com", "google.com"];
+
+	for server in &servers {
+		let ping = Command::new("ping")
+			.arg("-c")
+			.arg("1")
+			.arg(server)
+			.stdout(Stdio::null())
+			.stderr(Stdio::null())
+			.status();
+
+		match ping {
+			Ok(status) => {
+				if status.success() {
+					return 1;
+				}
+			}
+			Err(_) => return 0,
+		}
+	}
+	return 0;
 }
