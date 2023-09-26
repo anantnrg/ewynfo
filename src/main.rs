e653f06dcab53ea266085c16477326f6d984a610
Anant Narayan <anantnrg@gmail.com>
2023-09-26 15:36:21 +0530

    :sparkles: Arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
+use clap::Parser;
 use sysinfo::{
 	CpuExt,
 	System,
 	SystemExt,
 };
 
+#[derive(Parser, Default, Debug)]
+struct Arguments {
+	device: String,
+	#[clap(short, long)]
+	info: String,
+}
+
 fn main() {
 	let mut sys = System::new();
-	sys.refresh_cpu(); // Refreshing CPU information.
-	println!("{}% ", sys.global_cpu_info().cpu_usage());
+
+	let args = Arguments::parse();
+
+	parse_args(args, sys);
+}
+
+fn parse_args(args: Arguments, mut sys: System) {
+	match args.device.as_str() {
+		"cpu" => {
+			if args.info.as_str() == "usage" {
+				get_cpu_usage(sys);
+			} else if args.info.as_str() == "info" {
+				todo!();
+			}
+		}
+		&_ => {
+			println!("Unknown device!");
+		}
+	}
+}
+
+fn get_cpu_usage(mut sys: System) {
+	sys.refresh_cpu();
+	println!("{}%", sys.global_cpu_info().cpu_usage());
 }
